0fc17c20785491a8953121c8120859562f550fea
Antonio Valenti <valentiantonio89@gmail.com>
2021-01-10 19:29:44 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Lovelace generic html card by [@avalenti89](https://www.github.com/avalenti89)
+
+## Options
+
+| Name   | Type         | Requirement  | Description                  | Default |
+| ------ | ------------ | ------------ | ---------------------------- | ------- |
+| type   | string       | **Required** | `custom:generic-html-card`   |         |
+| title  | string       | **Optional** | Card title                   |         |
+| styles | string array | **Optional** | css string or stylesheet URI |         |
+| jss    | string array | **Optional** | javascript URI               |         |
---
+++ src/generic-html-card.ts
@@ -0,0 +1,182 @@
+import {
+  LitElement,
+  html,
+  customElement,
+  property,
+  TemplateResult,
+  PropertyValues,
+  internalProperty,
+} from "lit-element";
+import {
+  HomeAssistant,
+  LovelaceCardConfig,
+  LovelaceCard,
+  hasConfigOrEntityChanged,
+} from "custom-card-helpers"; // This is a community maintained npm module with common helper functions/types
+import { createCard } from "card-tools/src/lovelace-element";
+
+export interface GenericHtmlCardConfig extends LovelaceCardConfig {
+  id?: string;
+  className?: string;
+  styles?: string[];
+  jss?: string[];
+}
+
+// This puts your card into the UI card picker dialog
+(window as any).customCards = (window as any).customCards || [];
+(window as any).customCards.push({
+  type: "generic-html-card",
+  name: "Generic HTML Card",
+  description: "Generic HTLM Card",
+});
+
+@customElement("generic-html-card")
+export class GenericHtmlCard extends LitElement {
+  public static getStubConfig(): Record<string, unknown> {
+    return { cards: [] };
+  }
+
+  // https://lit-element.polymer-project.org/guide/properties
+  @property({ attribute: false }) public hass?: HomeAssistant;
+  @property() protected _cards?: LovelaceCard[];
+  @internalProperty() private config!: GenericHtmlCardConfig;
+
+  // https://lit-element.polymer-project.org/guide/properties#accessors-custom
+  public setConfig(config: GenericHtmlCardConfig): void {
+    // console.log("setConfig", this.config, config);
+    if (!config) {
+      throw new Error("Invalid configuration");
+    }
+    if (
+      (config.styles && !Array.isArray(config.styles)) ||
+      (config.jss && !Array.isArray(config.jss))
+    ) {
+      throw new Error("Invalid styles or jss");
+    }
+
+    this.config = {
+      name: "Generic html",
+      ...config,
+    };
+
+    this._cards =
+      config.cards?.map((card) => {
+        const element = this._createCardElement(card);
+        return element;
+      }) ?? [];
+  }
+
+  protected updated(changedProps: PropertyValues) {
+    // console.log("updated", this.config);
+
+    super.updated(changedProps);
+    if (!this._cards || !changedProps.has("hass")) {
+      return;
+    }
+
+    for (const element of this._cards) {
+      if (this.hass) {
+        element.hass = this.hass;
+      }
+    }
+  }
+
+  protected shouldUpdate(changedProps: PropertyValues): boolean {
+    console.log("shouldUpdate", this.config);
+    if (!this.config) {
+      return false;
+    }
+
+    const hasToChange = hasConfigOrEntityChanged(this, changedProps, false);
+    console.log("shoudlUpdate", hasToChange);
+    return hasToChange;
+  }
+
+  private _createCardElement(cardConfig: LovelaceCardConfig) {
+    // console.log("_createCardElement", this.config, cardConfig);
+    const element = createCard(cardConfig) as LovelaceCard;
+    // console.log(this.config?.id, this.config, cardConfig);
+    if (this.hass) {
+      element.hass = this.hass;
+    }
+    if (cardConfig.type === "custom:generic-html-card") {
+      if (cardConfig.className) {
+        element.className = cardConfig.className;
+      }
+      if (cardConfig.id) {
+        element.id = cardConfig.id;
+      }
+    }
+    element.addEventListener(
+      "ll-rebuild",
+      (ev) => {
+        ev.stopPropagation();
+        this._rebuildCard(element, cardConfig);
+      },
+      { once: true }
+    );
+    return element;
+  }
+
+  private _rebuildCard(
+    cardElToReplace: LovelaceCard,
+    config: LovelaceCardConfig
+  ): void {
+    // console.log("_rebuildCard", this.config, config);
+    const newCardEl = this._createCardElement(config);
+    if (cardElToReplace.parentElement) {
+      cardElToReplace.parentElement.replaceChild(newCardEl, cardElToReplace);
+    }
+    this._cards = this._cards!.map((curCardEl) =>
+      curCardEl === cardElToReplace ? newCardEl : curCardEl
+    );
+  }
+
+  // https://lit-element.polymer-project.org/guide/templates
+  protected render(): TemplateResult | void {
+    // console.log("render", this.config);
+    const styles =
+      this.config.styles?.reduce((prev, style) => {
+        if (
+          typeof style !== "string" ||
+          /^(http(s)?:\/\/)?(w{3}.)?.*\.css\??.*$/.test(style)
+        ) {
+          return prev;
+        }
+        return `${prev}${style}\n\n`;
+      }, "") ?? "";
+
+    let html_style: TemplateResult | undefined;
+    if (styles) {
+      html_style = html`<style>
+        ${styles}
+      </style>`;
+    }
+
+    const style_resources = this.config.styles?.reduce<TemplateResult[]>(
+      (prev, style) => {
+        if (
+          typeof style !== "string" ||
+          !/^(http(s)?:\/\/)?(w{3}.)?.*\.css\??.*$/.test(style)
+        ) {
+          return prev;
+        }
+
+        return [
+          ...prev,
+          html`<link type="text/css" rel="stylesheet" href="${style}" />`,
+        ];
+      },
+      []
+    );
+
+    return html`
+      ${html_style} ${style_resources}
+      ${this.config.jss?.map((js) => html`<script src="${js}"></script>`)}
+      ${this._cards}
+    `;
+  }
+  createRenderRoot() {
+    return this;
+  }
+}
